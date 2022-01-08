659f463139148e8c15366f20007eed8fe36c8190
Tonyaradzwa <tonyaradzwa@gmail.com>
2022-01-07 16:24:53 -0800

    Implemented the BallotBox Class

--- vote-counting-java/src/vote_counting_java/BallotBox.java
+++ vote-counting-java/src/vote_counting_java/BallotBox.java
@@ -1,27 +1,101 @@
 package vote_counting_java;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * This class stores the votes made by the voters
+ * The Ballot Box represents a particular region 
+ * of voters e.g voters in a particular district
+ * 
+ */
 public class BallotBox {
-	// your instance variables
-	public int votingPopulation;
-	public int numberOfCandidates;
 	
-	// your constructor
+	public int votingPopulation; // total population of voters in this region
+	public int numberOfCandidates; // number of candidates in the election
+	public String region; // name of voting region
+	public int regionNumber; // the identification number of the ballot box (we need to make sure that the number is not duplicable)
+	public File directory = new File("voteFiles");
+	public int numberOfVotes; // the total number of votes made in this region
+	
 	
 	/**
-	 * This creates a ballot box that stores all the votes
+	 * This creates a ballot box that stores voter region information 
+	 * and collects all the votes
+	 * 
+	 * @param votingPopulation population of voters in current region
+	 * @param region name of voting region
+	 * @param regionNumber id number of voting region 
 	 */
-	public BallotBox(int votingPopulation) {
+	public BallotBox(int votingPopulation, String region, int regionNumber) {
 		
 		this.votingPopulation = votingPopulation;
-		while (votingPopulation > 0)
-			System.out.println("Enter a vote from 1 to" + numberOfCandidates);
-			votingPopulation--;
+		this.region = region;
+		this.regionNumber = regionNumber;
+		
+		collectVotes();		
 	}
 	
-	// other useful methods
+	/**
+	 * This collects the votes and stores them in files
+	 * 
+	 */
+	public void collectVotes() {
+		File voteFile = new File(directory.getAbsolutePath() + File.separator + regionNumber + "_" + region + "_votes.txt");
+		// use this to print out the file path: System.out.println(voteFile.getAbsolutePath());
+		
+		try {
+			PrintWriter output = new PrintWriter(new FileOutputStream(voteFile));
+			
+			// first 2 lines in the file are the region number and region name 
+			output.println(regionNumber); 
+			output.println(region + "\n"); 
+
+			Scanner input = new Scanner(System.in);
+			int candidateNumber = 0;
+			
+			// this will ask each voter to enter a vote
+			for (int i = 0; i < votingPopulation; i++) {
+				System.out.println("For admin: enter a non-numeric character to close ballot ");
+				System.out.println("Enter the number of your candidate: ");
+				
+				
+				try {
+					candidateNumber = input.nextInt();
 	
+					// this automatically adds the vote receipt to the list of votes
+					output.println(new Vote(candidateNumber)); 	
+					numberOfVotes = i + 1; // the total number of votes made in this region
+					
+				// if a non-numeric character is entered by the admin, the ballot closes
+				} catch (InputMismatchException e) {
+					System.out.println("Ballot closed");
+				}
+			}
+			output.close();
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
-	// toString Method
+	/**
+	 * Prints out the number of votes made so far in the region.
+	 */
+	public String toString() {
+		
+		// return the number of votes made 
+		return "Total number of votes in region: " + numberOfVotes + "\nTotal number of votes: " + Vote.voteReceipts.size();
+	}
 	
 	// main method to test this class
+	public static void main(String args[]) {
+		
+		System.out.println(new BallotBox(3, "Area1", 1));
+		System.out.println(Vote.voteReceipts);
+		System.out.println(new BallotBox(2, "Area2", 2));
+		System.out.println(Vote.voteReceipts);
+	}
 }
